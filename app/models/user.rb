2d6545cec1c61f7c7478d8660cd0bf797ac58e39
Tomasz <twiduch@gmail.com>
2023-06-18 23:16:52 +0200

    creating new group on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :groups, dependent: :destroy
+  has_many :movies, -> { distinct }, through: :groups
+  belongs_to :active_group, class_name: 'Group'
+
+  before_validation -> { build_active_group(name: 'Initial', user: self) }, on: :create
 end
