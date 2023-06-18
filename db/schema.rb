3bbc5222d37313011e5440f59fba6b12d1462547
Tomasz <twiduch@gmail.com>
2023-06-18 17:42:47 +0200

    adding user

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_141353) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+end
