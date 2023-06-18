9b6b88ab26506e100a3f8be2e97e01e5c45e7f7a
Tomasz <twiduch@gmail.com>
2023-06-18 21:39:31 +0200

    rendering errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
+  rescue_from AppErrors::NotAuthorizedError do |e|
+    respond_to do |format|
+      format.html { render_error_page(401) }
+      format.json { render_error(e.message, 401) }
+    end
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    respond_to do |format|
+      format.html { render_error_page(404) }
+      format.json { render_error(e.message, 404) }
+    end
+  end
+
   def authenticate_request
     return true if current_user
 
@@ -11,5 +25,15 @@ class ApplicationController < ActionController::Base
 
   def user_id
     session[:user_id]
+  end
+
+  private
+
+  def render_error(msg, status)
+    render json: { status: 'error', message: msg }, status:
+  end
+
+  def render_error_page(code)
+    render file: "#{Rails.root.join("public/#{code}.html")}", layout: false, status: code
   end
 end
