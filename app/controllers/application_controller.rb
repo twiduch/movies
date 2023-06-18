565a86e7aa9d73f08dc29ee1a61c476a29848fad
Tomasz <twiduch@gmail.com>
2023-06-18 20:35:38 +0200

    request authentication function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  def authenticate_request
+    return true if current_user
+
+    raise AppErrors::NotAuthorizedError, 'Not authenticated'
+  end
+
+  def current_user
+    @current_user ||= user_id && User.find_by(id: user_id)
+  end
+
+  def user_id
+    session[:user_id]
+  end
 end
