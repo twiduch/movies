e61b546ef302132a8467dd771b9bc7a65f890465
Tomasz <twiduch@gmail.com>
2023-06-19 11:50:51 +0200

    adding movies_count to group

--- app/models/group_movie.rb
+++ app/models/group_movie.rb
@@ -1,5 +1,5 @@
 class GroupMovie < ApplicationRecord
-  belongs_to :group
+  belongs_to :group, counter_cache: :movies_count
   belongs_to :movie
 
   validates :group_id, uniqueness: { scope: :movie_id }
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,4 +4,40 @@ RSpec.describe Group do
   it 'is valid with valid attributes' do
     expect(group).to be_valid
   end
+
+  it 'has initial movies count 0' do
+    expect(group.movies_count).to eq 0
+  end
+
+  context 'when adding movie' do
+    let(:group) { create(:group, :with_user) }
+    let(:movie) { create(:movie) }
+
+    it 'updates movies_count' do
+      expect do
+        group.movies << movie
+      end.to change(group, :movies_count).by(1)
+    end
+  end
+
+  context 'when removing movie' do
+    let(:group) { create(:group, :with_user) }
+    let(:movie) { create(:movie) }
+
+    before do
+      group.movies << movie
+    end
+
+    it 'updates movies_count' do
+      expect do
+        group.movies.destroy(movie)
+      end.to change(group, :movies_count).by(-1)
+    end
+
+    it 'does not remove movie' do
+      expect do
+        group.movies.destroy(movie)
+      end.not_to change(Movie, :count)
+    end
+  end
 end
